bd7f8aff13a80d7d54df9d73e0b9ada7dd4a4ab3
iina18 <asihsri481@gmail.com>
2025-09-27 12:54:40 +0700

    ...

---
+++ Models/Exchange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmoni.Models
+{
+    var exchange = new Exchange
+    {
+        Id = 1,
+        MemberId = 101,
+        Member = memberObj, // objek Member yang sesuai
+        ExchangeId = "EXC20250927",
+        Amount = 100000m,
+        AmountExchanged = 95000m,
+        Fee = 5000m,
+        Rate = 0.95m,
+        ExchangeDate = DateTime.Now,
+        TotalAmountExchanged = 95000m
+    };
+
+    internal class Exchange
+    {
+    }
+}
