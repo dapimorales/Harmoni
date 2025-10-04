022c14bc95bcc7f3d4aa6fd7e848c78b7b0002cf
dapimorales <maulanakhadafi114@gmail.com>
2025-10-04 11:38:38 +0700

    menambahkan file admin form dan registration form

---
+++ Forms/Registration.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Harmoni.Forms
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtFullName = new TextBox();
+            txtCardId = new TextBox();
+            label2 = new Label();
+            txtEmail = new TextBox();
+            label3 = new Label();
+            txtFavColor = new TextBox();
+            label4 = new Label();
+            txtUsername = new TextBox();
+            label5 = new Label();
+            txtPhone = new TextBox();
+            label6 = new Label();
+            txtRefId = new TextBox();
+            label7 = new Label();
+            txtFavArtist = new TextBox();
+            label8 = new Label();
+            txtPassword = new TextBox();
+            label9 = new Label();
+            txtPhoneAlt = new TextBox();
+            label10 = new Label();
+            txtAddress = new TextBox();
+            label11 = new Label();
+            btnSubmit = new Button();
+            btnBack = new Button();
+            lblValidasi = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Full Name";
+            label1.Click += label1_Click;
+            // 
+            // txtFullName
+            // 
+            txtFullName.Location = new Point(31, 53);
+            txtFullName.Name = "txtFullName";
+            txtFullName.Size = new Size(299, 31);
+            txtFullName.TabIndex = 1;
+            // 
+            // txtCardId
+            // 
+            txtCardId.Location = new Point(31, 135);
+            txtCardId.Name = "txtCardId";
+            txtCardId.Size = new Size(299, 31);
+            txtCardId.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(31, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Card ID";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(31, 226);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(299, 31);
+            txtEmail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 198);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Email";
+            // 
+            // txtFavColor
+            // 
+            txtFavColor.Location = new Point(31, 318);
+            txtFavColor.Name = "txtFavColor";
+            txtFavColor.Size = new Size(299, 31);
+            txtFavColor.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(31, 290);
+            label4.Name = "label4";
+            label4.Size = new Size(198, 25);
+            label4.TabIndex = 6;
+            label4.Text = "What is Your Fav Color?";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(388, 53);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(296, 31);
+            txtUsername.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(388, 25);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 25);
+            label5.TabIndex = 8;
+            label5.Text = "Username";
+            // 
+            // txtPhone
+            // 
+            txtPhone.Location = new Point(388, 135);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(296, 31);
+            txtPhone.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(388, 107);
+            label6.Name = "label6";
+            label6.Size = new Size(62, 25);
+            label6.TabIndex = 10;
+            label6.Text = "Phone";
+            // 
+            // txtRefId
+            // 
+            txtRefId.Location = new Point(388, 226);
+            txtRefId.Name = "txtRefId";
+            txtRefId.Size = new Size(296, 31);
+            txtRefId.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(388, 198);
+            label7.Name = "label7";
+            label7.Size = new Size(111, 25);
+            label7.TabIndex = 12;
+            label7.Text = "Reference ID";
+            // 
+            // txtFavArtist
+            // 
+            txtFavArtist.Location = new Point(388, 318);
+            txtFavArtist.Name = "txtFavArtist";
+            txtFavArtist.Size = new Size(296, 31);
+            txtFavArtist.TabIndex = 15;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(388, 290);
+            label8.Name = "label8";
+            label8.Size = new Size(254, 25);
+            label8.TabIndex = 14;
+            label8.Text = "What is Your Fav Artist/Singer?";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(747, 53);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(307, 31);
+            txtPassword.TabIndex = 17;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(747, 25);
+            label9.Name = "label9";
+            label9.Size = new Size(87, 25);
+            label9.TabIndex = 16;
+            label9.Text = "Password";
+            // 
+            // txtPhoneAlt
+            // 
+            txtPhoneAlt.Location = new Point(747, 135);
+            txtPhoneAlt.Name = "txtPhoneAlt";
+            txtPhoneAlt.Size = new Size(307, 31);
+            txtPhoneAlt.TabIndex = 19;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(747, 107);
+            label10.Name = "label10";
+            label10.Size = new Size(89, 25);
+            label10.TabIndex = 18;
+            label10.Text = "Phone Alt";
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(747, 226);
+            txtAddress.Multiline = true;
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(307, 123);
+            txtAddress.TabIndex = 21;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(747, 198);
+            label11.Name = "label11";
+            label11.Size = new Size(77, 25);
+            label11.TabIndex = 20;
+            label11.Text = "Address";
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(31, 397);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(112, 34);
+            btnSubmit.TabIndex = 22;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            btnBack.Location = new Point(163, 397);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(112, 34);
+            btnBack.TabIndex = 23;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            // 
+            // lblValidasi
+            // 
+            lblValidasi.AutoSize = true;
+            lblValidasi.Location = new Point(306, 397);
+            lblValidasi.Name = "lblValidasi";
+            lblValidasi.Size = new Size(24, 25);
+            lblValidasi.TabIndex = 24;
+            lblValidasi.Text = "...";
+            lblValidasi.Click += label12_Click;
+            // 
+            // Registration
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1082, 582);
+            Controls.Add(lblValidasi);
+            Controls.Add(btnBack);
+            Controls.Add(btnSubmit);
+            Controls.Add(txtAddress);
+            Controls.Add(label11);
+            Controls.Add(txtPhoneAlt);
+            Controls.Add(label10);
+            Controls.Add(txtPassword);
+            Controls.Add(label9);
+            Controls.Add(txtFavArtist);
+            Controls.Add(label8);
+            Controls.Add(txtRefId);
+            Controls.Add(label7);
+            Controls.Add(txtPhone);
+            Controls.Add(label6);
+            Controls.Add(txtUsername);
+            Controls.Add(label5);
+            Controls.Add(txtFavColor);
+            Controls.Add(label4);
+            Controls.Add(txtEmail);
+            Controls.Add(label3);
+            Controls.Add(txtCardId);
+            Controls.Add(label2);
+            Controls.Add(txtFullName);
+            Controls.Add(label1);
+            Name = "Registration";
+            Text = "Registration";
+            Load += Registration_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtFullName;
+        private TextBox txtCardId;
+        private Label label2;
+        private TextBox txtEmail;
+        private Label label3;
+        private TextBox txtFavColor;
+        private Label label4;
+        private TextBox txtUsername;
+        private Label label5;
+        private TextBox txtPhone;
+        private Label label6;
+        private TextBox txtRefId;
+        private Label label7;
+        private TextBox txtFavArtist;
+        private Label label8;
+        private TextBox txtPassword;
+        private Label label9;
+        private TextBox txtPhoneAlt;
+        private Label label10;
+        private TextBox txtAddress;
+        private Label label11;
+        private Button btnSubmit;
+        private Button btnBack;
+        private Label lblValidasi;
+    }
+}
---
+++ Forms/Registration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harmoni.Forms
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Registration_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
