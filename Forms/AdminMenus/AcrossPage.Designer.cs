170905c5496181103b8409c0efdcea5f583160c2
adedwi208 <adeagustus02@gmail.com>
2025-11-29 12:25:33 +0700

    Tambah Across Page

---
+++ Forms/AdminMenus/AcrossPage.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Harmoni.Forms.AdminMenus
+{
+    partial class AcrossPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            memberToolStripMenuItem = new ToolStripMenuItem();
+            accessToolStripMenuItem = new ToolStripMenuItem();
+            produkToolStripMenuItem = new ToolStripMenuItem();
+            approvalToolStripMenuItem = new ToolStripMenuItem();
+            acrossCoorporationToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            label2 = new Label();
+            label3 = new Label();
+            dataGridView3 = new DataGridView();
+            button1 = new Button();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, memberToolStripMenuItem, accessToolStripMenuItem, produkToolStripMenuItem, approvalToolStripMenuItem, acrossCoorporationToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1209, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(50, 24);
+            fileToolStripMenuItem.Text = "File ";
+            // 
+            // memberToolStripMenuItem
+            // 
+            memberToolStripMenuItem.Name = "memberToolStripMenuItem";
+            memberToolStripMenuItem.Size = new Size(79, 24);
+            memberToolStripMenuItem.Text = "Member";
+            // 
+            // accessToolStripMenuItem
+            // 
+            accessToolStripMenuItem.Name = "accessToolStripMenuItem";
+            accessToolStripMenuItem.Size = new Size(67, 24);
+            accessToolStripMenuItem.Text = "Access";
+            // 
+            // produkToolStripMenuItem
+            // 
+            produkToolStripMenuItem.Name = "produkToolStripMenuItem";
+            produkToolStripMenuItem.Size = new Size(69, 24);
+            produkToolStripMenuItem.Text = "Produk";
+            // 
+            // approvalToolStripMenuItem
+            // 
+            approvalToolStripMenuItem.Name = "approvalToolStripMenuItem";
+            approvalToolStripMenuItem.Size = new Size(84, 24);
+            approvalToolStripMenuItem.Text = "Approval";
+            // 
+            // acrossCoorporationToolStripMenuItem
+            // 
+            acrossCoorporationToolStripMenuItem.Name = "acrossCoorporationToolStripMenuItem";
+            acrossCoorporationToolStripMenuItem.Size = new Size(159, 24);
+            acrossCoorporationToolStripMenuItem.Text = "Across Coorporation";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Coop";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(21, 102);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(528, 193);
+            dataGridView1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(595, 102);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.Size = new Size(527, 191);
+            dataGridView2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(595, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Member";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 328);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Transfer";
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(23, 368);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.RowHeadersWidth = 51;
+            dataGridView3.Size = new Size(759, 160);
+            dataGridView3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(861, 476);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 42);
+            button1.TabIndex = 7;
+            button1.Text = "Refresh";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // AcrossPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button1);
+            Controls.Add(dataGridView3);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            Name = "AcrossPage";
+            Size = new Size(1209, 571);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem memberToolStripMenuItem;
+        private ToolStripMenuItem accessToolStripMenuItem;
+        private ToolStripMenuItem produkToolStripMenuItem;
+        private ToolStripMenuItem approvalToolStripMenuItem;
+        private ToolStripMenuItem acrossCoorporationToolStripMenuItem;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Label label2;
+        private Label label3;
+        private DataGridView dataGridView3;
+        private Button button1;
+    }
+}
---
+++ Forms/AdminMenus/AcrossPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harmoni.Forms.AdminMenus
+{
+    public partial class AcrossPage : UserControl
+    {
+        public AcrossPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
